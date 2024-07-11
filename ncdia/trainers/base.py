113b06e8eff7a3b38c038ec6cef9178cbc95ec8f
JackMa01 <2337303340@qq.com>
2024-07-11 09:28:38 +0800

    add trainers.optim

--- ncdia/trainers/base.py
+++ ncdia/trainers/base.py
@@ -1,9 +1,24 @@
-
+from torch.utils.data import DataLoader
 
 
 class BaseTrainer(object):
-    """
+    """Basic trainer class for training models.
+
+    Args:
+
     
     """
-    def __init__(self):
+    def __init__(
+            self,
+            max_epoch: int,
+            train_loader: DataLoader,
+    ):
         super(BaseTrainer, self).__init__()
+        self.max_epoch = max_epoch
+        self.train_loader = train_loader
+
+    def train(self):
+        """
+        
+        """
+        self.total_iters = self.max_epoch * len(self.train_loader)
---
+++ ncdia/trainers/optim/optimizer.py
@@ -0,0 +1,48 @@
+import torch
+import torch.nn as nn
+
+
+def build_optimizer(
+        name: str,
+        model: nn.Module,
+        param_groups: dict | None = None,
+        **kwargs
+    ) -> torch.optim.Optimizer:
+    """Build optimizer.
+
+    Args:
+        name (str): name of optimizer
+        model (nn.Module | dict): model or param_groups
+        param_groups (dict | None): 
+            if provided, directly optimize param_groups and abandon model
+        **kwargs: arguments for optimizer
+
+    Returns:
+        optimizer (torch.optim.Optimizer): optimizer
+
+    Raises:
+        NotImplementedError: optimizer not supported
+
+    Examples:
+        >>> model = nn.Linear(10, 1)
+        >>> optimizer = build_optimizer('adam', model)    
+    """
+    if param_groups is None:
+        if isinstance(model, nn.Module):
+            param_groups = model.parameters()
+        else:
+            param_groups = model
+
+    name = name.lower()
+    if name == 'adam':
+        optimizer = torch.optim.Adam(param_groups, **kwargs)
+    elif name == 'adamw':
+        optimizer = torch.optim.AdamW(param_groups, **kwargs)
+    elif name == 'sgd':
+        optimizer = torch.optim.SGD(param_groups, **kwargs)
+    elif name == 'rmsprop':
+        optimizer = torch.optim.RMSprop(param_groups, **kwargs)
+    else:
+        raise NotImplementedError(f"Optimizer {name} not supported")
+
+    return optimizer
