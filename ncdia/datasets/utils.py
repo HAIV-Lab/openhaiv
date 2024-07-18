6687f6eee3b0daa4da11267bf374c4042efd216f
lzwqjh <1244295148@qq.com>
2024-07-18 15:14:50 +0800

    update dataloader

--- ncdia/datasets/utils.py
+++ ncdia/datasets/utils.py
@@ -2,7 +2,7 @@ from ncdia.utils import Config
 from .get_dataloader import get_cil_dataloader2
 
 def get_dataloader(config: Config):
-    if config.dataloader.name == 'savc_att_dataloader':
+    if config.dataloader.name == 'remote_att_dataloader':
         return get_cil_dataloader2
-    elif config.dataloader.name == 'savc_dataloader':
+    elif config.dataloader.name == 'remote_dataloader':
         return get_cil_dataloader2
