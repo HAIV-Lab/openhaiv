cb743cea1d750b30d276cfc6db5635921195ff51
JackMa01 <2337303340@qq.com>
2024-07-22 11:02:34 +0800

    add

--- ncdia/datasets/dataloader.py
+++ ncdia/datasets/dataloader.py
@@ -1,4 +1,5 @@
 from torch.utils.data import DataLoader
+from torchvision.transforms import transforms
 
 from ncdia.utils import DATASETS
 
@@ -20,7 +21,15 @@ def build_dataloader(kwargs):
     if 'dataset' not in kwargs:
         raise ValueError("Dataset configuration not provided")
     
-    dataset_cfg = kwargs.pop('dataset')
+    dataset_cfg = dict(kwargs.pop('dataset'))
+    dataset_cfg['transform'] = transforms.Compose([
+        transforms.Resize((256, 256)),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(
+            mean=[0.485, 0.456, 0.406],
+            std=[0.229, 0.224, 0.225])
+    ])
     dataset = DATASETS.build(dataset_cfg)
 
     loader = DataLoader(dataset, **kwargs)
