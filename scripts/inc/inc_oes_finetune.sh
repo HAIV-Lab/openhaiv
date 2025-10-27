86bfedb5c17938dccb9609dd7ab486ab1290cc4f
YifanLiang-hust <yfl@hust.edu.cn>
2025-10-27 16:27:27 +0800

    remove unused files

--- scripts/inc/inc_oes_finetune.sh
+++ scripts/inc/inc_oes_finetune.sh
@@ -2,6 +2,6 @@
 # Model: ResNet18
 # Method: Finetune
 # Task: Class-incremental Learning
-CUDA_VISIBLE_DEVICES=2 python train.py \
+python train.py \
     --cfg configs/pipeline/incremental_learning/inc_oes_finetune.yaml \
     --opts device='cuda:0'
